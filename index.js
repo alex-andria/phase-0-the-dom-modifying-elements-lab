010e2c9955540d8afa48b2458e6f5b8050e85bfc
Alexandria DAntonio <dantonio.alexandria@gmail.com>
2021-12-02 02:47:53 -0500

    commit tree 94ba9b3af81ae8b0701fa683b762ce5fb76fcd0b

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-// Write your code here!
-document.getElementById('main').remove();
+document.querySelector('main').remove();
 
+const body = document.querySelector('body');
 
 const newHeader = document.createElement('h1');
-newHeader.id = 'victory';
 
+newHeader.id = 'victory';
 newHeader.innerHTML = "Alex is the champion!";
+body.append(newHeader);
